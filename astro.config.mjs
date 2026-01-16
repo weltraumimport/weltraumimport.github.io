5bba8b8cc370283c249027d5db66f2ccc1558d01
weltraumimport <jessressler@gmx.de>
2026-01-17 00:39:44 +0100

    astro introduction finished

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
-export default defineConfig({site: 'https://weltraumimport.github.io/',});
+export default defineConfig({
+  site: 'https://weltraumimport.github.io/',
+  integrations: [preact()],
+});
